6efe7d745508552d13bc6923b1396f45d20e07d2
SergeyBudnik <serbudnik@gmail.com>
2019-09-19 08:48:04 +0300

    .

--- src/app/pages/cabinet/timetable/cabinet-timetable.page.ts
+++ src/app/pages/cabinet/timetable/cabinet-timetable.page.ts
@@ -4,6 +4,7 @@ import {LoginService} from '../../../service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Cabinet, Group, Lesson} from '../../../data';
 import {CabinetsHttp, GroupsHttp} from '../../../http';
+import {endOfWeek, startOfWeek} from 'date-fns';
 
 @Component({
   selector: 'app-cabinet-timetable-page',
@@ -12,6 +13,7 @@ import {CabinetsHttp, GroupsHttp} from '../../../http';
 })
 export class CabinetTimetablePageComponent extends TranslatableComponent {
   public cabinet: Cabinet = new Cabinet(null, null, null);
+  private allGroups: Array<Group> = [];
 
   public cabinetGroups: Array<Group> = [];
   public cabinetLessons: Array<Lesson> = [];
@@ -45,22 +47,47 @@ export class CabinetTimetablePageComponent extends TranslatableComponent {
       this.groupsHttp.getAllGroups()
     ]).then(it => {
       this.cabinet = it[0];
-      this.initTimetable(cabinetId, it[1]);
+      this.allGroups = it[1];
+
+      this.initTimetable(cabinetId, this.allGroups, new Date());
 
       this.loadingInProgress = false;
     });
   }
 
+  public onWeekChanged(currentWeek: number) {
+    const time = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000 * currentWeek);
+
+    this.initTimetable(this.cabinet.id, this.allGroups, time);
+  }
+
   private initTimetable(
     cabinetId: number,
-    allGroups: Array<Group>
+    allGroups: Array<Group>,
+    time: Date
   ): void {
+    const options = {weekStartsOn: 1};
+
+    const weekStartTime = startOfWeek(time, options).getTime() + 9 * 60 * 60 * 1000;
+    const weekEndTime = endOfWeek(time, options).getTime();
+
+    const cabinetGroups: Array<Group> = [];
+    const cabinetLessons: Array<Lesson> = [];
+
     allGroups
       .filter(group => group.cabinetId === cabinetId)
       .forEach(group => {
-        this.cabinetGroups.push(group);
+        cabinetGroups.push(group);
 
-        group.lessons.forEach(lesson => this.cabinetLessons.push(lesson));
+        group.lessons
+          .filter(lesson => lesson.creationTime <= weekStartTime)
+          .filter(lesson => !lesson.deactivationTime || weekEndTime <= lesson.deactivationTime)
+          .forEach(lesson => {
+            cabinetLessons.push(lesson)
+          });
       });
+
+    this.cabinetGroups = cabinetGroups;
+    this.cabinetLessons = cabinetLessons;
   }
 }
